5b2385657f95104b4606edfe5732daabfb8b7efb
saeedkokash <saeedkokash@gmail.com>
2022-08-10 23:37:05 +0300

    dont with last Two Labs

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,4 @@
+import { getByDisplayValue } from '@testing-library/react';
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
@@ -8,8 +9,8 @@ class Header extends React.Component {
   render() {
     return (
       <div className="navBar">
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="dark" >
-          <Navbar.Brand >My Favorite Books</Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="dark" style={{display: 'flex', justifyContent: 'space-around', color: 'black'}}>
+          <Navbar.Brand style={{color: 'black'}}>My Favorite Books</Navbar.Brand>
           <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
           <NavItem><Link to="/profile" className="nav-link">Profile</Link></NavItem>
           <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
---
+++ src/components/BookFormModal.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+export default class BookFormModal extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+    }
+
+    handleClose = () => {
+        this.setState({
+            show: false
+        })
+    }
+    handleShow = () => {
+        this.setState({
+            show: true
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <Button variant="primary" onClick={this.handleShow}> Add Book </Button>
+
+                <Modal show={this.state.show} onHide={this.handleClose} >
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add Book to your Collection</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <Form onSubmit={this.props.submitHandler}>
+                            <Form.Group className="mb-3" controlId="bookTitle">
+                                <Form.Label>Book Title</Form.Label>
+                                <Form.Control type="text" placeholder="Book name" autoFocus />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="bookDescription" >
+                                <Form.Label>Book Description</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="bookImgURL" >
+                                <Form.Label>Book Image imgURL</Form.Label>
+                                <Form.Control type="text" placeholder="Book Image URL" autoFocus />
+                            </Form.Group>
+
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                                <Button type='submit' variant="primary" onClick={this.handleClose}>Save Changes</Button>
+                            </Modal.Footer>
+                        </Form>
+                    </Modal.Body>
+
+                </Modal>
+            </>
+        );
+
+    }
+}
+
